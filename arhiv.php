9e4d6d08414eb75a54a287ee2e517c4299c80638
Tovarish <vadim@asus-K50AB.(none)>
2013-05-03 13:25:06 +0300

    add

---
+++ arhiv.php
@@ -0,0 +1,23 @@
+<?php
+include('./parser.php');
+$db=mysql_connect ("localhost","root","root");
+mysql_select_db("local_wiki_analytics",$db);
+foreach (glob("/var/www/access.log.*") as $file) {
+    openAccessLog($file);
+    //echo "<br/>===================================<br/><br/>";
+}
+mysql_close($db);
+function openAccessLog($fileName) {
+	//$fileName = "/var/www/access.log.2.gz";
+	$fp = gzopen($fileName, "r");
+
+	ob_start();
+		gzpassthru($fp);
+		$string = ob_get_contents();
+	ob_end_clean();
+	parser($string);
+	//echo $string;
+	//echo gzread($fp, filesize($fileName));
+	gzclose($fp);
+}
+
--- parser.php
+++ parser.php
@@ -1,39 +1,57 @@
 <?php
-$string = file_get_contents('/var/www/access.log.1');
-
-preg_match_all('/(.*?) - - \[(.*?)\] "(.*?)" ([0-5]{3}) ([0-9]*) "(.*?)" "(.*?)"/', $string, $result);
-
-$query = "insert into `access_log` (`client_IP`, `datatime`, `request_line`, `status_code`,`size_object_returned`, `referer`, `user_agent`) values ";
-
-foreach($result[1] as $key => $resIp){
-	$items[$key] = "('{$resIp}', ";
-}
-foreach($result[2] as $key => $resDatetime){
-	$items[$key] .= "'{$resDatetime}', ";
-}
-foreach($result[3] as $key => $resRequestLine){
-	$items[$key] .= "'{$resRequestLine}', ";
-}
-foreach($result[4] as $key => $resStatusCode){
-	$items[$key] .= "'{$resStatusCode}', ";
-}
-foreach($result[5] as $key => $resSizeObjectReturned){
-	$items[$key] .= "'{$resSizeObjectReturned}', ";
-}
-foreach($result[6] as $key => $resReferer){
-	$items[$key] .= "'{$resReferer}', ";
-}
-foreach($result[7] as $key => $resUserAgent){
-	$items[$key] .= "'{$resUserAgent}'),";
-}
-foreach($items as $item){
-	$query .= $item;
-}
-$query = rtrim($query, ',');
-
 $db=mysql_connect ("localhost","root","root");
 mysql_select_db("local_wiki_analytics",$db);
-mysql_query($query);
+$result = mysql_fetch_row(mysql_query("select size from file_attributes"));
+$oldSize = $result[0];
+//$oldTime = 1367496733;
+$newSize = filesize('/var/www/access.log');
+//echo $oldSize." = ".$newSize;die;
+//$newTime = filemtime('/var/www/access.log');
+//echo $newTime;
+if($newSize > $oldSize) {
+	$string = file_get_contents('/var/www/access.log', null, null, $oldSize, $newSize);
+	parser($string);
+	$query = "update file_attributes set size = {$newSize} where id = 1";
+	mysql_query($query);
+} elseif($newSize < $oldSize) {
+	$query = "update file_attributes set size = 0 where id = 1";
+	mysql_query($query);
+}
 mysql_close($db);
+function parser($string) {
+	//$string = file_get_contents('/var/www/access.log');
 
-//127.0.0.1 - - [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+	preg_match_all('/(.*?) - - \[(.*?)\] "(.*?)" ([0-5]{3}) ([0-9]*) "(.*?)" "(.*?)"/', $string, $result);
+
+	$query = "insert into `access_log` (`client_IP`, `datatime`, `request_line`, `status_code`,`size_object_returned`, `referer`, `user_agent`) values ";
+
+	foreach($result[1] as $key => $resIp){
+		$items[$key] = "('{$resIp}', ";
+	}
+	foreach($result[2] as $key => $resDatetime){
+		$items[$key] .= "'{$resDatetime}', ";
+	}
+	foreach($result[3] as $key => $resRequestLine){
+		$items[$key] .= "'{$resRequestLine}', ";
+	}
+	foreach($result[4] as $key => $resStatusCode){
+		$items[$key] .= "'{$resStatusCode}', ";
+	}
+	foreach($result[5] as $key => $resSizeObjectReturned){
+		$items[$key] .= "'{$resSizeObjectReturned}', ";
+	}
+	foreach($result[6] as $key => $resReferer){
+		$items[$key] .= "'{$resReferer}', ";
+	}
+	foreach($result[7] as $key => $resUserAgent){
+		$items[$key] .= "'{$resUserAgent}'),";
+	}
+	foreach($items as $item){
+		$query .= $item;
+	}
+	$query = rtrim($query, ',');
+
+	mysql_query($query);
+
+	//127.0.0.1 - - [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+}
